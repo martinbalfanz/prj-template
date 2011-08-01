a581cee7822a1fdbe2a710eb0d7df78bbdf3c784
Martin Balfanz <martin.balfanz@googlemail.com>
2011-08-01 18:17:46 +0200

    enhanced configuration

--- freeze.py
+++ freeze.py
@@ -3,6 +3,11 @@ from prj import app
 
 freezer = Freezer(app)
 
+app.config.update(
+    # FREEZER_BASE_URL = '',
+    FREEZER_REMOVE_EXTRA_FILES = True
+)
+
 if __name__ == '__main__':
     freezer.freeze()
     freezer.serve()
