df07563b321989260b74db78771626a7fb59242f
Martin Balfanz <martin.balfanz@googlemail.com>
2011-08-01 18:38:13 +0200

    removed unnecessary debug configuration

--- prj.py
+++ prj.py
@@ -3,11 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-app.config['DEBUG'] = DEBUG = True
+app.config['DEBUG'] = True
 
 @app.route('/')
 def home():
-    return render_template('home.html', page_id='page-home', debug=DEBUG)
+    return render_template('home.html', page_id='page-home')
 
 if __name__ == '__main__':
     app.run()
